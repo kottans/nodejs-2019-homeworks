c944389a354ac85b8d62436fad9be2c224ba035d
eoneoff <eoneoff@gmail.com>
2019-09-28 19:09:01 +0300

    implemented suggested improvements adn added function import

--- submissions/eoneoff/port-sniffer/sniffer.js
+++ submissions/eoneoff/port-sniffer/sniffer.js
@@ -2,38 +2,63 @@
 
 "use strict";
 
-let help = {
+const help = {
     script:`Simple port sniffer
 
-This is a simple port sniffer, which can be calles as command line script or imported as a module.
+This is a simple port sniffer, which can be called as command line script or imported as a module.
 When used from command line it takes 2 named paramentes:
 --host      obligatory parameter which defines the host address for a port scan.
             After the --host parameter should follow the host IP addres as four numbers,
             separated by commas or a host web address.
 
-                python sniffer.py --host www.google.com
+                node sniffer.js --host www.google.com
 
 --ports     optional parementer to provide a port range for scan. The range
-            should be definded as two number with a dash between them. If not set
+            should be defined as two number with a dash between them. If not set
             or set incorrectly the default range of (1-65535) is scanned
 
-                python sniffer.py --host wwww.google.com --ports 1024-2018
+                node sniffer.js --host wwww.google.com --ports 1024-2018
 
---help      parameted to see this reference
+--help      parameter to see this reference
 
 To import port scanner use
 
-    import sniffer.py
+    const sniffer = require('sniffer.js')
 
 after import you can perform scan by calling sniffer.scan()
 
-scan(host, portString, module, silent)
+sniffer.scan(host[, portString][, args])
 
-For  detailed disambiguation of scan function use help(scan) after importing
-sniffer or call sniffer.py with --help method`
+For detailed disambiguation on the function call sniffer.help()`,
+function: `Simple port scanning method
+
+This is an async function for port scanning. It takes one obligatory argument 'host' and two
+optional arguments 'portString', and additional arguments object 'args' with 'module' and
+'silent' properties.
+
+    sniffer.scan(host[, portString][, args])
+
+host:str        an obligatory argument, which provies a host for scanning. It can be an
+                IP address as string of four numbers, separated by dots or a hostname
+
+posrtString:str an optional argument to define a range of scanned ports. Should be provided
+                as a string, composed of two numbers, separated by dash. If not provided or
+                provided incorrectly the port range will be set to default full range (1-65535)
+
+args:Object                
+
+    module:bool     a bool argument, which tells the function to run as a module, that is to
+                    return a result as a list of open ports
+
+    silent:bool     a bool argument, which tells the function to supress the output and run
+                    in silent mode. Takes effect only if 'modlue' parameter is also set
+                    to 'true'
+
+The function is async and returns a promice. You shoud eather use it with await operator or
+with then() method`
 }
 
-const dns = require('dns');
+const dns = require('dns').promises;
 const Socket = require('net').Socket;
 
 function* twister(){
@@ -45,18 +70,27 @@ function* twister(){
     }
 }
 
+function isIpAddress(address){
+    return address.search(/^((25[0-5]|2[0-4]\d|[01]?\d?\d)\.){3}(25[0-5]|2[0-4]\d|[01]?\d?\d)$/) == -1;
+}
+
+function isRange(ip) {
+    return ip.search(/\d{1,4}-\d{1,4}/) == -1;
+}
+
+function isInvalidPortsRange(portsRange){
+    return +portsRange[0] >= +portsRange[1] || +portsRange[0] < 1 || +portsRange[0] > 65535
+    || +portsRange[1] < 1 || +portsRange[1]>65535;
+}
+
 async function parseHost(hostName){
-    let host = ''
-    if(hostName.search(/^((25[0-5]|2[0-4]\d|[01]?\d?\d)\.){3}(25[0-5]|2[0-4]\d|[01]?\d?\d)$/) == -1){
-        host = await new Promise((resolve, reject)=>{
-            dns.lookup(hostName, (error, address) => {
-                if(error){
-                    error.message = "Hostname can not be resolved";
-                    reject(error);
-                }
-                resolve(address)
-            });
-        });
+    let host = '';
+    if(isIpAddress(hostName)){
+        try{
+            host = (await dns.lookup(hostName)).address;
+        } catch {
+            throw "Hostname can not be resolved";
+        }
     } else {
         host = hostName;
     }
@@ -64,12 +98,11 @@ async function parseHost(hostName){
 }
 
 function parsePorts(ports){
-    if(ports.search(/\d{1,4}-\d{1,4}/) == -1){
+    if(isRange(ports)){
         throw "Invalid port range argument"
     } else {
         let limits = ports.split("-");
-        if (+limits[0] >= +limits[1] || +limits[0] < 1 || +limits[0] > 65535
-            || +limits[1] < 1 || +limits[1]>65535){
+        if (isInvalidPortsRange(limits)){
                 throw "Port numbers out of range"
             } else {
                 limits[1]++;
@@ -81,11 +114,11 @@ function parsePorts(ports){
 async function parseArguments(args){
     let host = "";
     let ports = [1, 65536];
-    let hostIndex = args.indexOf('--host');
-    if(hostIndex == -1 || hostIndex == args.length -1){
+    let hostIndex = args.indexOf('--host') + 1;
+    if(hostIndex == 0 || hostIndex == args.length){
         throw "You must specify a host to scan ports";
     } else {
-        host = await parseHost(args[hostIndex + 1]);
+        host = await parseHost(args[hostIndex]);
     }
     let portsIndex = args.indexOf('--ports') + 1;
     if (portsIndex != 0){
@@ -100,7 +133,7 @@ async function parseArguments(args){
         }
     }
 
-    return {host:host, ports:ports}
+    return {host, ports}
 }
 
 async function scan(host, ports, args = {silent:false,module:false}){
@@ -139,14 +172,22 @@ async function scan(host, ports, args = {silent:false,module:false}){
         if(openPorts.length){
             result = `Port${openPorts.length > 1 ? 's': ''} ${openPorts.join(', ')} ${openPorts.length > 1 ? 'are' : 'is'}`
         } else{
-            result = 'No ports are'
+            result = 'No ports are';
         }
 
         console.log(` \n${result} open`);
     }
+
+    if(args.module) return openPorts;
 }
 
-if(process.argv.indexOf('--help') != -1){
+module.exports.scan = async (host, ports = "", args={module:true, silent:false}) =>{
+    return await scan(await parseHost(host), parsePorts(ports), args);
+};
+
+module.exports.help = () => console.log(help.function);
+
+if(process.argv.includes('--help')){
     console.log(help.script);
 } else {
     try {
