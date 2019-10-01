8d56e9cfa4df3946ef10e73db13522b7e0dd7cfb
Zakkaray <bloomzakk@gmail.com>
2019-10-02 00:26:24 +0300

    Review changes

--- submissions/Zakkarat/port-sniffer/sniffer.js
+++ submissions/Zakkarat/port-sniffer/sniffer.js
@@ -1,12 +1,13 @@
 const Net = require("net");
-
+const dns = require("dns").promises;
+const args = process.argv.slice(2);
 const help = `
 This script used for finding open ports for specified host.
 
 Paramaters: 
-    --host     Paramater which specifies adress, where ports will be searched.
-               Adress must be specified after the parameter.
-               It can be specified as IP adress or URL.
+    --host     Paramater which specifies address, where ports will be searched.
+               Address must be specified after the parameter.
+               It can be specified as IP address or URL.
                Examples: --host google.com,
                --host 172.217.3.110
     
@@ -18,104 +19,110 @@ Paramaters:
                --port 10-99
 
     --help     Parameter will show this instructions. Hope they were useful!
-`
-
-const toObject = args => {
-  const obj = {};
-  args.forEach((elem, i) => {
-    if (elem === '--help') {
-        console.log(help);
-        obj.help = true;
-    }else if (i % 2 === 0) {
-      elem = elem.slice(2);
-      obj[elem] = args[i + 1];
-    }
-  });
-  return obj;
-};
+`;
 
 const checkHost = async host => {
-  if(!host) {
-    console.log('No host specified. Use --help for more information.')
+  if (!host) {
+    console.log("No host specified. Use --help for more information.");
   }
-  const dns = require("dns").promises;
-  let adress = await dns
+  return await dns
     .lookup(host)
     .then(res => {
       return res.address;
     })
     .catch(error => {
       console.log("Invalid hostname. Use --help for more information.");
-      return 0;
     });
-  return adress;
 };
 
 const checkPorts = port => {
-    let portLimits = [1, 65535];
-    if(port) {
-        portLimits = port.split("-").map(elem => Number(elem))
-        if ((0 > portLimits[0] || 65535 < portLimits[1]) || portLimits[2]) {
-          console.log("Invalid port range. Use --help for more information.");
-          return [0];
-        }
-    } else {
-          console.log('No port range specified. Setting to default. For more information try --help')
+  let portLimits = [1, 65535];
+  if (port) {
+    portLimits = port.split("-").map(elem => Number(elem));
+    if (1 > portLimits[0] || 65535 < portLimits[1] || portLimits[2]) {
+      throw new Error("Invalid port range. Use --help for more information.");
     }
-    return portLimits;
-}
-
-const checkValid = async (host, port) => {
-  let adress = await checkHost(host);
-  let ports = checkPorts(port);
-  return [adress, ...ports];
+  } else {
+    console.log("No port range specified. Setting to default. For more information try --help")
+  }
+  return portLimits;
 };
 
-const sniff = async ({ host, port, help }) => {  
-    if(help) {
-        return 0;
-    } else if(!host && !port) {
-        console.log('No arguments specified. Try --help for more information.')
-        return 0;
-    }
-  const hostPort = await checkValid(host, port);
-  let openPorts = [];
-  if (hostPort.every(elem => elem)) {
-    for (let i = hostPort[1]; i <= hostPort[2]; i++) {
-      try {
-        await new Promise((resolve, reject) => {
-          const client = new Net.Socket();
-          client.setTimeout(300);
-          client.on("connect", () => {
-            process.stdout.write('.')
-            openPorts.push(i);
-            client.destroy();
-            resolve();
-          });
-          client.on("timeout", () => {
-            client.destroy();
-            reject();
-          });
-          client.on("error", () => {
-            client.destroy();
-            reject();
-          });
-          client.connect({
-            port: i,
-            host: hostPort[0]
-          });
+const checkValid = async (host, port) => {
+  let address = await checkHost(host);
+  let ports;
+  try {
+    ports = checkPorts(port);
+  } catch(e) {
+    console.log(e);
+    ports = [0];
+  }
+  if([address, ...ports].every(elem => elem)) {
+  return [address, ...ports];
+  } else {
+    process.exit(1);
+  }
+};
+
+const checkConnection = async ([host, minPort, maxPort]) => {
+  const openPorts = [];
+    for (let i = minPort; i <= maxPort; i++) {
+    try {
+      await new Promise((resolve, reject) => {
+        const client = new Net.Socket();
+        client.setTimeout(300);
+        client.on("connect", () => {
+          process.stdout.write(".");
+          openPorts.push(i);
+          client.destroy();
+          resolve();
         });
-      } catch {}
-    }
+        client.on("timeout", () => {
+          client.destroy();
+          reject();
+        });
+        client.on("error", () => {
+          client.destroy();
+          reject();
+        });
+        client.connect({
+          port: i,
+          host: host
+        });
+      });
+    } catch {}
+  }
+  return openPorts;
+}
+
+const sniff = async ({ host, port, help }) => {
+  if (help) {
+    return 0;
+  } else if (!host && !port) {
+    console.log("No arguments specified. Try --help for more information.");
+    return 0;
+  }
+  const hostPort = await checkValid(host, port);
+  const openPorts = await checkConnection(hostPort);
+  if (hostPort.every(elem => elem)) {
+
   } else {
     return 0;
   }
-  process.stdout.write('\n')
-  openPorts.forEach(elem => openPorts.indexOf(elem) === openPorts.length - 1 ? 
-      process.stdout.write(`${elem}`) :
-      process.stdout.write(`${elem},`)
-      )
-  process.stdout.write(' ports are opened')
+  openPorts.length ? console.log(`\n${openPorts.join(', ')} ports are opened`) : console.log(`No ports opened`)
 };
 
-sniff(toObject(process.argv.slice(2)));
+
+
+sniff(
+  args.reduce((obj, elem, i) => {
+    if (elem === "--help") {
+      console.log(help);
+      obj.help = true;
+    } else if (i % 2 === 0) {
+      elem = elem.slice(2);
+      obj[elem] = args[i + 1];
+    }
+    return obj;
+  }, {})
+);
