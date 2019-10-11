9631d50a0c3fca8155331f20fd4f5aa0e6667692
eoneoff <eoneoff@gmail.com>
2019-10-11 19:27:32 +0300

    comment explaining api added to main.js

--- submissions/eoneoff/data-structures-http/main.js
+++ submissions/eoneoff/data-structures-http/main.js
@@ -1,3 +1,35 @@
+/*
+Data structues with html assess
+
+    The assess to data structues is provided through html requests
+
+    stack
+        <socket>/stack
+
+        POST request with json object of format {"data":<value to push to stack>} pushes vaule to stack
+
+        DELETE request pops a value from a stack and returns in in json object of format
+        {"data": <value popped from stack}
+
+    linked list
+        <socket>/list
+
+        GET request returns the contents of list in a json object
+
+        POST request with json object of format
+        {
+            "data": <value to insert into stack>[,
+            "sucessor": <successor value>]
+        }
+        inserts value into list
+
+        DELETE request with json object of format {"data":<value to remove from list>} removes
+        the value from the list
+
+        If data is not presented in json format, if it is not string or number or if a successor
+        or an item to remove is not in the list a Bad Reques code is returned
+*/
+
 const { Server } = require('./server/server');
 const { LinkedList } = require('./data_structures/linkedList');
 const { Stack } = require('./data_structures/stack');
