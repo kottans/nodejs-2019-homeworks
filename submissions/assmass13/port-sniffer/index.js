3430a9962368d1314a153b973b316a65f3822c8e
root <root@localhost.localdomain>
2019-10-16 09:52:02 -0400

    refactoring

--- submissions/assmass13/port-sniffer/index.js
+++ submissions/assmass13/port-sniffer/index.js
@@ -1,37 +1,42 @@
-const dns = require('dns')
+const dns = require('dns');
 
-const requiredArguments = ['host']
-const defaultPortLimits = {
-    'start_port': 0,
-    'end_port': 65535
-}
-const helpMessage = `hey bitch`
+// const requiredArguments = ['host'];
+// const defaultPortLimits = {
+//   start_port: 0,
+//   end_port: 65535
+// };
+const helpMessage = `hey bitch`;
 
-function combineArgs(args) { 
-    return args.reduce((result, item, index, arr) => {
-        if (item.indexOf('--') === 0 && arr[index + 1])
-            result[item] = arr[index + 1]
-        return result
-    }, {})
+function stdoutWrite(str) {
+  process.stdout.write(str);
 }
 
- async function getAddress(host) {
-    return new Promise((resolve, reject) => {
-        dns.lookup(host, (err, address) => {
-            if (err) reject(err)
-            resolve(address)
-        })
-    }).catch(e => console.log('\nError occured while proccessing', host))
+function combineArgs(args) {
+  return args.reduce((result, item, index, arr) => {
+    const newResult = { ...result };
+    if (item.indexOf('--') === 0 && arr[index + 1])
+      newResult[item] = arr[index + 1];
+    return newResult;
+  }, {});
 }
 
+async function getAddress(host) {
+  return new Promise((resolve, reject) => {
+    dns.lookup(host, (err, address) => {
+      if (err) reject(err);
+      resolve(address);
+    });
+  }).catch(e => {
+    stdoutWrite(`Error ${e.code} in ${e.syscall} for ${e.hostname}\n`);
+  });
+}
 
-(async function() {
-    const processArgs = process.argv.slice(2)
-    const combinedArgs = combineArgs(processArgs)
-    if (!combinedArgs.hasOwnProperty('--host')) {
-        console.log(helpMessage)
-        process.exit(1)
-    }
-    await getAddress(combinedArgs['--host'])
-    // console.log(await getAddress(combinedArgs['--host']))
-})()
+(async function sniff() {
+  const processArgs = process.argv.slice(2);
+  const combinedArgs = combineArgs(processArgs);
+  if (!combinedArgs['--host']) {
+    stdoutWrite(helpMessage);
+    process.exit(1);
+  }
+  stdoutWrite(await getAddress(combinedArgs['--host']));
+})();
