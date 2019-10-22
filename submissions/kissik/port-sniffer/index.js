92a17295da62f1d157ca10d11da11110d4cd9dcb
kissik <karayuziv@gmail.com>
2019-10-22 19:12:25 +0300

    Fix eslint errors

--- submissions/kissik/port-sniffer/index.js
+++ submissions/kissik/port-sniffer/index.js
@@ -1,43 +1,41 @@
-"use strict";
+const net = require('net');
 
-const net = require("net");
-const argp = require("commander");
-var async = require("async");
-var res = "";
-var st, ed;
-var completed = 0;
+const argp = require('commander');
 
-argp
-  .version("1.0.0")
-  .option("-H, --host <host>", "ip-v4 or domain.name", "google.com")
-  .option("-c, --connect <cnt>", "symbol", ".")
-  .option("-e, --error <cnt>", "symbol", "e")
-  .option("-u, --unableConnect <uncnt>", "symbol", "!")
-  .option(
-    "-t, --timeout <tout>",
-    "timeout in milliseconds of inactivity on the socket",
-    300
-  )
-  .option("-p, --ports <port>", "ports 1-65535", "80");
+const async = require('async');
 
-argp.parse(process.argv);
-
-print(argp.opts());
-
-st = parseInt(argp.ports.split("-")[0]);
-ed = parseInt(
-  argp.ports.split("-").length == 1
-    ? argp.ports.split("-")[0]
-    : argp.ports.split("-")[1]
-);
-
-print(`scaning ${argp.host}:${st}-${ed}`);
+let res = '';
+let completed = 0;
 
 function print(x) {
   if (x.length) process.stdout.write(x);
 }
+argp
+  .version('1.0.0')
+  .option('-H, --host <host>', 'ip-v4 or domain.name', 'google.com')
+  .option('-c, --connect <cnt>', 'symbol', '.')
+  .option('-e, --error <cnt>', 'symbol', 'e')
+  .option('-u, --unableConnect <uncnt>', 'symbol', '!')
+  .option(
+    '-t, --timeout <tout>',
+    'timeout in milliseconds of inactivity on the socket',
+    300
+  )
+  .option('-p, --ports <port>', 'ports 1-65535', '80');
 
-var scanPorts = function(lsts) {
+argp.parse(process.argv);
+
+const st = parseInt(argp.ports.split('-')[0], 10);
+const ed = parseInt(
+  argp.ports.split('-').length === 1
+    ? argp.ports.split('-')[0]
+    : argp.ports.split('-')[1],
+  10
+);
+
+print(`scaning ${argp.host}:${st}-${ed}`);
+
+function scanPorts(lsts) {
   const client = new net.Socket();
 
   function done(err, con) {
@@ -45,15 +43,13 @@ var scanPorts = function(lsts) {
 
     if (con) {
       print(argp.connect);
-      res += res ? "," + lsts.port : lsts.port;
+      if (res) res += `,${lsts.port}`;
+      else res += lsts.port;
     } else print(argp.unableConnect);
-
-    if (++completed === ed - st + 1) {
-      print("\n" + (res ? res : "none") + " port(s) is(are) opened\n");
-      return;
+    completed += 1;
+    if (completed === ed - st + 1) {
+      print(`\n${res !== '' ? res : 'none'} port(s) is(are) opened\n`);
     }
-
-    return;
   }
 
   client
@@ -62,39 +58,40 @@ var scanPorts = function(lsts) {
       client.end();
       client.destroy();
     })
-    .setTimeout(parseInt(argp.timeout), () => {
+    .setTimeout(parseInt(argp.timeout, 10), () => {
       done(null, false);
       client.end();
       client.destroy();
     })
-    .on("data", () => {
+    .on('data', () => {
       done(null, true);
       client.end();
       client.destroy();
     })
-    .on("end", () => {
+    .on('end', () => {
       done(null, true);
       client.end();
       client.destroy();
     })
-    .on("error", err => {
+    .on('error', err => {
       done(err, false);
       client.end();
       client.destroy();
     });
-};
+}
 
 function sniffer(callback) {
-  let q = async.queue(scanPorts, 65535);
+  const q = async.queue(scanPorts, 65535);
+  let lsts;
 
-  for (let i = st; i <= ed; i++) {
-    var lsts = {};
+  for (let i = st; i <= ed; i += 1) {
+    lsts = {};
     lsts.port = i;
     lsts.host = argp.host;
     q.push(lsts);
   }
 
-  callback("\nwell async done\n");
+  callback('\nwell async done\n');
 }
 
 sniffer(print);
