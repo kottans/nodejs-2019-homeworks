d16970236e2c9da8789374a08fb53cce0293d975
kissik <karayuziv@gmail.com>
2019-10-23 12:25:24 +0300

    Fixes respectively to PR 'Port sniffer' #11,
    https://github.com/kottans/nodejs-2019-homeworks/pull/11

--- submissions/kissik/port-sniffer/index.js
+++ submissions/kissik/port-sniffer/index.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const argp = require('commander');
+const argumentParser = require('commander');
 
 const async = require('async');
 
@@ -10,7 +10,7 @@ let completed = 0;
 function print(x) {
   if (x.length) process.stdout.write(x);
 }
-argp
+argumentParser
   .version('1.0.0')
   .option('-H, --host <host>', 'ip-v4 or domain.name', 'google.com')
   .option('-c, --connect <cnt>', 'symbol', '.')
@@ -23,53 +23,43 @@ argp
   )
   .option('-p, --ports <port>', 'ports 1-65535', '80');
 
-argp.parse(process.argv);
+argumentParser.parse(process.argv);
 
-const st = parseInt(argp.ports.split('-')[0], 10);
-const ed = parseInt(
-  argp.ports.split('-').length === 1
-    ? argp.ports.split('-')[0]
-    : argp.ports.split('-')[1],
+const startingPort = parseInt(argumentParser.ports.split('-')[0], 10);
+const endingPort = parseInt(
+  argumentParser.ports.split('-').length === 1
+    ? argumentParser.ports.split('-')[0]
+    : argumentParser.ports.split('-')[1],
   10
 );
 
-print(`scaning ${argp.host}:${st}-${ed}`);
+print(`scaning ${argumentParser.host}:${startingPort}-${endingPort}`);
 
-function scanPorts(lsts) {
+function scanPorts({ port, host }) {
   const client = new net.Socket();
 
   function done(err, con) {
-    if (err) print(argp.error);
+    if (err) print(argumentParser.error);
 
     if (con) {
-      print(argp.connect);
-      if (res) res += `,${lsts.port}`;
-      else res += lsts.port;
-    } else print(argp.unableConnect);
+      print(argumentParser.connect);
+      if (res) res += `,${port}`;
+      else res += port;
+    } else print(argumentParser.unableConnect);
     completed += 1;
-    if (completed === ed - st + 1) {
-      print(`\n${res !== '' ? res : 'none'} port(s) is(are) opened\n`);
+    if (completed === endingPort - startingPort + 1) {
+      print(`\n${res || 'none'} port(s) is(are) opened\n`);
     }
   }
 
   client
-    .connect({ port: lsts.port, host: lsts.host }, () => {
+    .connect({ port, host }, () => {
       done(null, true);
       client.end();
       client.destroy();
     })
-    .setTimeout(parseInt(argp.timeout, 10), () => {
+    .setTimeout(parseInt(argumentParser.timeout, 10), () => {
       done(null, false);
-      client.end();
-      client.destroy();
-    })
-    .on('data', () => {
-      done(null, true);
-      client.end();
-      client.destroy();
-    })
-    .on('end', () => {
-      done(null, true);
       client.end();
       client.destroy();
     })
@@ -82,15 +72,8 @@ function scanPorts(lsts) {
 
 function sniffer(callback) {
   const q = async.queue(scanPorts, 65535);
-  let lsts;
-
-  for (let i = st; i <= ed; i += 1) {
-    lsts = {};
-    lsts.port = i;
-    lsts.host = argp.host;
-    q.push(lsts);
-  }
-
+  for (let i = startingPort; i <= endingPort; i += 1)
+    q.push({ port: i, host: argumentParser.host });
   callback('\nwell async done\n');
 }
 
